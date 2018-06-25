8805e07963ceedaa06dd58bcc89ae1338d2cb136
tomas ignacio pereyra <tomaspereyra1997@gmail.com>
2018-06-25 19:04:47 -0300

    pagina con todas las vistas

--- BlogNoticias/Controllers/HomeController.cs
+++ BlogNoticias/Controllers/HomeController.cs
@@ -17,14 +17,17 @@ namespace BlogNoticias.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
 
+            return View();
+        }
+
+        public IActionResult Post()
+        {
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
 
             return View();
         }
