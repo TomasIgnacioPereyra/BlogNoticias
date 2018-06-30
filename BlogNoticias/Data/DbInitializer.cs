3a77655632d3666dbc7da31dc3d6f6b12410f25d
tomas ignacio pereyra <tomaspereyra1997@gmail.com>
2018-06-29 23:24:54 -0300

    pagina parcialmente terminada

--- BlogNoticias/Data/DbInitializer.cs
+++ BlogNoticias/Data/DbInitializer.cs
@@ -18,8 +18,8 @@ namespace BlogNoticias.Data
             }
             var publicacion = new Publicacion[]
             {
-                new Publicacion{Titulo="curso de .NET",Subtitulo="c#, core 2, javascript",Autor="ELVER GALARGA",Fecha=new DateTime(2018,06,10),Cuerpo="mi primer blog"},
-                new Publicacion{Titulo="curso 111mil",Subtitulo="java",Autor="Benito Camelo",Fecha=new DateTime(2018,06,10),Cuerpo="Mi primer blog"}
+                new Publicacion{Titulo="curso de .NET",Subtitulo="c#, core 2, javascript",Autor="Mauro Icardi",Fecha=new DateTime(2018,06,10),Cuerpo="mi primer blog"},
+                new Publicacion{Titulo="curso 111mil",Subtitulo="java",Autor="Mauro Icardi",Fecha=new DateTime(2018,06,10),Cuerpo="Mi primer blog"}
 
             };
             foreach (Publicacion c in publicacion)
--- BlogNoticias/Models/Publicacion.cs
+++ BlogNoticias/Models/Publicacion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,14 +10,22 @@ namespace BlogNoticias.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
         public string Titulo { get; set; }
 
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
         public string Subtitulo { get; set; }
 
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
         public string Autor { get; set; }
-
+        [Required]
         public DateTime Fecha { get; set; }
 
+        [Required]
+        [StringLength(5000, MinimumLength = 100)]
         public string Cuerpo { set; get; }
 
     }
